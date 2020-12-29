111291162ec8aa808dbf05441037689e5217403d
Karl Lee <69225621+KarlLeeS@users.noreply.github.com>
2020-12-29 13:49:43 +0900

    Create useInput2.js

---
+++ hooks/useInput2.js
@@ -0,0 +1,18 @@
+import {useState} from "react"; 
+
+const Useinput = (init="")=>{
+    // input 데이터를 다루기 위한 state 값 
+    const [value,setValue] = useState(init); 
+
+    // input 태그의 onChange 속성에 넣을 function. onchange 마다 매 state 최신화 
+    const onChange =e=>{
+        const {
+            target :{value} 
+        } = e; 
+        setValue(value); 
+    }
+    return {value,onChange,setValue};
+}
+
+export default Useinput;
+
