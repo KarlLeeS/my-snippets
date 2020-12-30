fd25fd15b436dfec2468da37c81b4c60994a78f6
Karl Lee <69225621+KarlLeeS@users.noreply.github.com>
2020-12-30 09:48:19 +0900

    Update useAction.js

--- hooks/useAction.js
+++ hooks/useAction.js
@@ -18,3 +18,38 @@ export default function useActions(actions, deps) {
     deps ? [dispatch, ...deps] : deps
   );
 }
+ 
+//////////////////////////////////////////////////////////////////////
+// usage  
+//////////////////////////////////////////////////////////////////////
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default React.memo(TodosContainer);
